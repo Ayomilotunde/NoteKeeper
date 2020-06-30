2c7add1ca998a589078402850049694a25ef687c
Ayomilotunde <51304919+Ayomilotunde@users.noreply.github.com>
2020-06-30 05:59:33 +0100

    update

---
+++ app/src/main/java/com/ayomi/notekeeper/NoteActivityViewModel.java
@@ -0,0 +1,9 @@
+package com.ayomi.notekeeper;
+
+import androidx.lifecycle.ViewModel;
+
+class NoteActivityViewModel extends ViewModel {
+    public String mOriginalNoteCourseId;
+    public String mOriginalNoteTitle;
+    public String mOriginalNoteText;
+}
